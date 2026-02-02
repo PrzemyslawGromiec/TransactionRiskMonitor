d45e1bb24cc61969a56b292a4adbc28096d6e490
pgromiec <pgromiec@gmail.com>
2026-02-02 18:45:23 +0000

    Initial Spring Boot project setup

---
+++ src/main/java/org/example/transactionriskmonitor/TransactionRiskMonitorApplication.java
@@ -0,0 +1,13 @@
+package org.example.transactionriskmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionRiskMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionRiskMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/transactionriskmonitor/TransactionRiskMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.transactionriskmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionRiskMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
