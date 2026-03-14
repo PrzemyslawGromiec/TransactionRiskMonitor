c0884f72bc0568cabe7621223e289b339fd71537
pgromiec <pgromiec@gmail.com>
2026-03-14 12:04:28 +0000

    REFACTOR: delete unused class

--- src/main/java/org/example/transactionriskmonitor/domain/service/RiskAssessment.java
+++
@@ -1,9 +0,0 @@
-package org.example.transactionriskmonitor.domain.service;
-
-import org.example.transactionriskmonitor.domain.model.RiskReason;
-import org.example.transactionriskmonitor.domain.model.RiskScore;
-
-import java.util.EnumSet;
-
-public record RiskAssessment(RiskScore score, EnumSet<RiskReason> reasons) {
-}
